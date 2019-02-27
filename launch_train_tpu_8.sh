91ca7f8cac8b9cf049ff6a8a8f7dd6bc909f2b6f
David Mack <david@octavian.ai>
2019-02-27 22:18:14 +0000

    c

--- launch_train_tpu_8.sh
+++ launch_train_tpu_8.sh
@@ -4,8 +4,8 @@ python3.6 main_tpu.py --use-tpu --tpu-name node-1 \
 	--train-input-path gs://octavian-static/download/pgan/atk-vclose-128.tfrecords \
 	--model-dir gs://octavian-training2/pgan/model \
 	--batch-size 256  \
-	--verbosity WARNING \
-	--steps-per-loop 1000 \
+	--verbosity INFO \
+	--steps-per-loop 100 \
 	--train-steps 1000 \
 	--eval-steps 100 \
 
