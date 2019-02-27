7e0945bc30a0ff94fd4142902cbed4618e1697a7
David Mack <david@sketchdeck.com>
2019-02-27 11:19:18 -0800

    smaller

--- launch_train_tpu_small.sh
+++ launch_train_tpu_small.sh
@@ -3,5 +3,8 @@
 python3.6 main_tpu.py --use-tpu --tpu-name node-1 \
 	--train-input-path gs://octavian-static/download/pgan/atk-vclose-128.tfrecords \
 	--model-dir gs://octavian-training2/pgan/model \
-	--batch-size 32  \
-	--verbosity INFO
+	--batch-size 16  \
+	--verbosity INFO \
+	--steps-per-loop 10 \
+	--train-steps 10 \
+
