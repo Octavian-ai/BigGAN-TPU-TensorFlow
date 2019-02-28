53c772a182cff7c3e1f6415aa6d857002a418562
David Mack <david@sketchdeck.com>
2019-02-27 20:31:55 -0800

    profile

--- launch_tpu_profile.sh
+++ launch_tpu_profile.sh
@@ -1,1 +1,1 @@
-pipenv run capture_tpu_profile --tpu=node-1  --monitoring_level=1 --tpu_zone us-central1-f
+pipenv run capture_tpu_profile --tpu="${TPU_NAME:-node-1}"  --monitoring_level=1 --tpu_zone us-central1-f
