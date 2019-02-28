44c85dc24744d6be9510d924679e2445a59f4166
David Mack <david@sketchdeck.com>
2019-02-27 20:34:01 -0800

    shrink a bit

--- launch_train_tpu_8.sh
+++ launch_train_tpu_8.sh
@@ -3,7 +3,7 @@
 nohup pipenv run python3.6 main_tpu.py --use-tpu --tpu-name "${TPU_NAME:-node-1}" \
 	--train-input-path gs://octavian-static/download/pgan/atk-vclose-128.tfrecords \
 	--model-dir gs://octavian-training2/pgan/model \
-	--batch-size 512  \
+	--batch-size 256  \
 	--verbosity INFO \
 	--steps-per-loop 3000 \
 	--train-steps 3000 \
