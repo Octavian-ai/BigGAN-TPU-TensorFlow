f28f774f4975e8381cac08d72366cf158db20cd7
David Mack <david@sketchdeck.com>
2019-02-27 20:24:59 -0800

    fixes

--- launch_train_tpu_8.sh
+++ launch_train_tpu_8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup pipenv run python3.6 main_tpu.py --use-tpu --tpu-name "${TPU_NODE:-node-1}" \
+nohup pipenv run python3.6 main_tpu.py --use-tpu --tpu-name "${TPU_NAME:-node-1}" \
 	--train-input-path gs://octavian-static/download/pgan/atk-vclose-128.tfrecords \
 	--model-dir gs://octavian-training2/pgan/model \
 	--batch-size 512  \
@@ -8,5 +8,5 @@ nohup pipenv run python3.6 main_tpu.py --use-tpu --tpu-name "${TPU_NODE:-node-1}
 	--steps-per-loop 3000 \
 	--train-steps 3000 \
 	--eval-steps 10 \
-	$@
+	$@ &
 	
