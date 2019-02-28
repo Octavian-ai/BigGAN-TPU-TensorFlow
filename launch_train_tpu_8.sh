a53898b60b2b75425a01497585add33a34177e8c
David Mack <david@sketchdeck.com>
2019-02-27 20:08:03 -0800

    var tpu

--- launch_train_tpu_8.sh
+++ launch_train_tpu_8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3.6 main_tpu.py --use-tpu --tpu-name node-1 \
+python3.6 main_tpu.py --use-tpu --tpu-name "${TPU_NODE:-node-1}" \
 	--train-input-path gs://octavian-static/download/pgan/atk-vclose-128.tfrecords \
 	--model-dir gs://octavian-training2/pgan/model \
 	--batch-size 512  \
