e1e0659d4482c90d10ef21a9d616550b5e2c9cd5
David Mack <david@sketchdeck.com>
2019-03-08 12:21:59 -0800

    fix eval

--- launch_train_tpu_smoke_test.sh
+++ launch_train_tpu_smoke_test.sh
@@ -2,7 +2,7 @@
 
 nohup pipenv run python3.6 main_tpu.py --use-tpu \
 	--train-input-path gs://octavian-static/download/imagenet/tfr-full/train* \
-	--eval-input-path gs://octavian-static/download/imagenet/tfr-full/validate* \
+	--eval-input-path gs://octavian-static/download/imagenet/tfr-full/validation* \
 	--model-dir gs://octavian-training2/gan/imagenet/model \
 	--result-dir ./results \
 	--batch-size 32  \
