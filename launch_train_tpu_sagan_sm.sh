23003bc2ae967ede23f48c9f56e278667cf5d378
David Mack <david@sketchdeck.com>
2019-03-14 11:12:16 -0700

    add script

---
+++ launch_train_tpu_sagan_sm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+nohup pipenv run python3.6 main_tpu.py --use-tpu \
+	--train-input-path gs://octavian-static/download/imagenet/tfr-full/train* \
+	--eval-input-path gs://octavian-static/download/imagenet/tfr-full/validation* \
+	--model-dir gs://octavian-training2/gan/imagenet/model \
+	--result-dir ./results \
+	--batch-size 256  \
+	--steps-per-loop 100 \
+	--ch 64 \
+	--layers 3 \
+	--img-size 32 \
+	--self-attn-res 16 \
+	--g-lr 0.0001 \
+	--d-lr 0.0004 \
+	--verbosity INFO \
+	--train-steps 9375 \
+	--eval-steps 391 \
+	--tag sagan-sm \
+	--tag run-$RANDOM \
+	$@ &
+	
