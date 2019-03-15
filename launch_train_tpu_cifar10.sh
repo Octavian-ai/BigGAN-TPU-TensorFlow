634d7e42f85d586a81f884bc6020f4d2c22f2317
David Mack <david@sketchdeck.com>
2019-03-15 15:43:30 -0700

    now with cifar10

--- launch_train_tpu_cifar10.sh
+++ launch_train_tpu_cifar10.sh
@@ -3,19 +3,19 @@
 nohup pipenv run python main_tpu.py \
 	--use-tpu \
 	--tpu-name $TPU_NAME \
-	--model-dir gs://octavian-training2/gan/mnist/model \
-	--data-dir gs://octavian-training2/gan/mnist/data \
+	--model-dir gs://octavian-training2/gan/cifar10/model \
+	--data-dir gs://octavian-training2/gan/cifar10/data \
 	--result-dir ./results \
 	--data-source mnist \
-	--img-size 28 \
-	--img-ch 1 \
+	--img-size 32 \
+	--img-ch 3 \
 	--num-labels 10 \
 	--steps-per-loop 500 \
 	--train-examples 60000 \
 	--eval-examples 10000 \
 	--layers 3 \
 	--epoch 20 \
-	--tag mnist \
+	--tag cifar10 \
 	--tag run-$RANDOM \
 	--disable-inception-score \
 	$@ &
